2e3b94e45ac18a81c16eff2e765a01d164ca1f3e
0xMerso <merso.eth@gmail.com>
2025-04-08 23:55:54 +0200

    ops sh

--- update.sh
+++ update.sh
@@ -3,7 +3,13 @@
 # For dev purposes, to update the app
 
 echo "Getting submodules ..."
-git pull --recurse-submodules
-git submodule update --remote --recursive
+# git pull --recurse-submodules
+# git submodule update --remote --recursive
+cd sdk
+git pull origin mai
+cd ../front
+git pull origin main
+cd ..
+
 docker compose stop
 docker compose up --build -d
