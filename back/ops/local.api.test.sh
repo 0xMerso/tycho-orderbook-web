78e1a1fe389f99937171fae2d5bcdecfa5387efb
0xMerso <merso.eth@gmail.com>
2025-05-19 12:49:13 +0200

    Misc

--- back/ops/local.api.test.sh
+++ back/ops/local.api.test.sh
@@ -98,12 +98,12 @@ try "GET /$network/pairs" "$API_URL/$network/pairs"
 
 # Test simulations
 try "POST /$network/orderbook (simple)" "$API_URL/$network/orderbook" '{"tag": "'"$eth-$usdc"'"}'
-try "POST /$network/orderbook (simple)" "$API_URL/$network/orderbook" '{"tag": "'"$eth-$wbtc"'"}'
-try "POST /$network/orderbook (simple)" "$API_URL/$network/orderbook" '{"tag": "'"$eth-$dai"'"}'
+# try "POST /$network/orderbook (simple)" "$API_URL/$network/orderbook" '{"tag": "'"$eth-$wbtc"'"}'
+# try "POST /$network/orderbook (simple)" "$API_URL/$network/orderbook" '{"tag": "'"$eth-$dai"'"}'
 # try "POST /$network/orderbook (simple)" "$API_URL/$network/orderbook" '{"tag": "'"$eth-$usdt"'"}'
 # try "POST /$network/orderbook (simple)" "$API_URL/$network/orderbook" '{"tag": "'"$usdc-$wbtc"'"}'
-# try "POST /$network/orderbook (simple)" "$API_URL/$network/orderbook" '{"tag": "'"$usdc-$dai"'"}'
-# try "POST /$network/orderbook (simple)" "$API_URL/$network/orderbook" '{"tag": "'"$usdc-$usdt"'"}'
+try "POST /$network/orderbook (simple)" "$API_URL/$network/orderbook" '{"tag": "'"$usdc-$dai"'"}'
+try "POST /$network/orderbook (simple)" "$API_URL/$network/orderbook" '{"tag": "'"$usdc-$usdt"'"}'
 # try "POST /$network/orderbook (simple)" "$API_URL/$network/orderbook" '{"tag": "'"$wbtc-$dai"'"}'
 # try "POST /$network/orderbook (simple)" "$API_URL/$network/orderbook" '{"tag": "'"$wbtc-$usdt"'"}'
 
